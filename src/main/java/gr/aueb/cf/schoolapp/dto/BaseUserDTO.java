5e87a2430c93c36938fac6c406f7a7f24074d4f8
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-06 14:10:32 +0300

    feat: default constuctor added on BaseUserDTO

--- src/main/java/gr/aueb/cf/schoolapp/dto/BaseUserDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/BaseUserDTO.java
@@ -14,6 +14,7 @@ public abstract class BaseUserDTO {
     private String password;
     private String confirmedPassword;
 
+    public BaseUserDTO() {}
 
     public BaseUserDTO(String username, String password) {
         this.username = username;
