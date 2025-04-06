22367f2069d8eef77ac87d8731625fff2c143234
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-06 14:07:27 +0300

    initial commit

--- src/main/java/gr/aueb/cf/schoolapp/controller/LoginController.java
+++ src/main/java/gr/aueb/cf/schoolapp/controller/LoginController.java
@@ -1,17 +1,17 @@
 package gr.aueb.cf.schoolapp.controller;
 
-//import gr.aueb.cf.schoolapp.authentication.AuthenticationProvider;
-//import gr.aueb.cf.schoolapp.dao.ITeacherDAO;
-//import gr.aueb.cf.schoolapp.dao.IUserDAO;
-//import gr.aueb.cf.schoolapp.dao.TeacherDAOImpl;
-//import gr.aueb.cf.schoolapp.dao.UserDAOImpl;
-//import gr.aueb.cf.schoolapp.exceptions.UserDAOException;
-//import gr.aueb.cf.schoolapp.dto.UserLoginDTO;
-//import gr.aueb.cf.schoolapp.service.ITeacherService;
-//import gr.aueb.cf.schoolapp.service.IUserService;
-//import gr.aueb.cf.schoolapp.service.TeacherServiceImpl;
-//import gr.aueb.cf.schoolapp.service.UserServiceImpl;
-//import gr.aueb.cf.schoolapp.exceptions.UserNotFoundException;
+import gr.aueb.cf.schoolapp.authentication.AuthenticationProvider;
+import gr.aueb.cf.schoolapp.dao.ITeacherDAO;
+import gr.aueb.cf.schoolapp.dao.IUserDAO;
+import gr.aueb.cf.schoolapp.dao.TeacherDAOImpl;
+import gr.aueb.cf.schoolapp.dao.UserDAOImpl;
+import gr.aueb.cf.schoolapp.exceptions.UserDAOException;
+import gr.aueb.cf.schoolapp.dto.UserLoginDTO;
+import gr.aueb.cf.schoolapp.service.ITeacherService;
+import gr.aueb.cf.schoolapp.service.IUserService;
+import gr.aueb.cf.schoolapp.service.TeacherServiceImpl;
+import gr.aueb.cf.schoolapp.service.UserServiceImpl;
+import gr.aueb.cf.schoolapp.exceptions.UserNotFoundException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -24,14 +24,14 @@ import java.io.IOException;
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
 
-//    private final IUserDAO userDAO = new UserDAOImpl();    //todo
-//    private final IUserService userService = new UserServiceImpl(userDAO); //todo
+    private final IUserDAO userDAO = new UserDAOImpl();
+    private final IUserService userService = new UserServiceImpl(userDAO);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-//        String isError = request.getParameter("isError");
-//        request.setAttribute("isError", isError == null ? "false" : "true");
+        String isError = request.getParameter("isError");
+        request.setAttribute("isError", isError == null ? "false" : "true");
 
         request.getRequestDispatcher("WEB-INF/jsp/login.jsp").forward(request, response);
     }
@@ -43,45 +43,45 @@ public class LoginController extends HttpServlet {
         // Data binding
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-//        UserLoginDTO userLoginDTO = new UserLoginDTO(username, password); //todo
+        UserLoginDTO userLoginDTO = new UserLoginDTO(username, password);
 
         boolean principleIsAuthenticated = false;
-//        try {
-//            principleIsAuthenticated = AuthenticationProvider.authenticate(userLoginDTO); //todo
-//
-//            if (!principleIsAuthenticated) {
-//                request.setAttribute("error", "Invalid credentials");
-//                request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
-//                return;
-//            }
-//
-//            HttpSession oldSession = request.getSession(false);
-//            // no session exists before
-//            // we explicitly create a session with getSession(true)
-//            // that is that tomcat creates a jsessionid, but does not store it to backend
-//            // session object. it is just a placeholder. to create a session object and
-//            // thus create state at backend you have to call getSession(true) or getSession()
-//            if (oldSession != null) {
-//                oldSession.invalidate(); // Destroy attacker's session
-//            }
-//            HttpSession session = request.getSession(true);  // Create new one for address fixation attack
-//            session.setAttribute("authenticated", true);
-//            session.setAttribute("username", username);
-//            session.setAttribute("role", userService.getUserByUsername(username).getRoleType().name()); //todo
-//
-//            if (session.getAttribute("role").equals("ADMIN")) { // overwrites web.xml
-//                session.setMaxInactiveInterval(ADMIN_TIMEOUT);  // Admin get 30-min sessions
-//            }
-//
-//            response.sendRedirect(request.getContextPath() + "/school-app/dashboard");
-//
-//
-////                response.sendRedirect(request.getContextPath() + "/login?isError=true");
-//
-//        } catch (UserDAOException | UserNotFoundException e) { //todo
-//            //response.sendRedirect(request.getContextPath() + "/login?isError=true");
-//            request.setAttribute("error", "Authentication Error");
-//            request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
-//        }
+        try {
+            principleIsAuthenticated = AuthenticationProvider.authenticate(userLoginDTO);
+
+            if (!principleIsAuthenticated) {
+                request.setAttribute("error", "Invalid credentials");
+                request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
+                return;
+            }
+
+            HttpSession oldSession = request.getSession(false);
+            // no session exists before
+            // we explicitly create a session with getSession(true)
+            // that is that tomcat creates a jsessionid, but does not store it to backend
+            // session object. it is just a placeholder. to create a session object and
+            // thus create state at backend you have to call getSession(true) or getSession()
+            if (oldSession != null) {
+                oldSession.invalidate(); // Destroy attacker's session
+            }
+            HttpSession session = request.getSession(true);  // Create new one for address fixation attack
+            session.setAttribute("authenticated", true);
+            session.setAttribute("username", username);
+            session.setAttribute("role", userService.getUserByUsername(username).getRoleType().name());
+
+            if (session.getAttribute("role").equals("ADMIN")) { // overwrites web.xml
+                session.setMaxInactiveInterval(ADMIN_TIMEOUT);  // Admin get 30-min sessions
+            }
+
+            response.sendRedirect(request.getContextPath() + "/school-app/dashboard");
+
+
+//                response.sendRedirect(request.getContextPath() + "/login?isError=true");
+
+        } catch (UserDAOException | UserNotFoundException e) {
+            //response.sendRedirect(request.getContextPath() + "/login?isError=true");
+            request.setAttribute("error", "Authentication Error");
+            request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
+        }
     }
 }
