270043edd9bb34d50474b9c893e6496d4a9a3bb8
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-23 19:36:17 +0300

    chore: lombok removed

--- src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
@@ -1,9 +1,5 @@
 package gr.aueb.cf.schoolapp.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 public class TeacherInsertDTO extends BaseTeacherDTO {
 
